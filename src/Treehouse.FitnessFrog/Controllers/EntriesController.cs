bb5f9cf62967b99f7fd165f8524732bc368dc695
Jaime Jesus Guerra Torres <jaimicox@gmail.com>
2017-08-28 16:44:15 -0500

    Updated the entries controller add method parameter data types

--- src/Treehouse.FitnessFrog/Controllers/EntriesController.cs
+++ src/Treehouse.FitnessFrog/Controllers/EntriesController.cs
@@ -46,10 +46,11 @@ namespace Treehouse.FitnessFrog.Controllers
 
         //[ActionName("Add"), HttpPost]
         [HttpPost]
-        public ActionResult Add(string date, string activityId, string duration, string intensity, string exclude, string notes)
+        public ActionResult Add(DateTime? date, int? activityId, double? duration, Entry.IntensityLevel? intensity, bool? exclude, string notes)
         {
             // Este codigo extra la informacion del campo del formulario. requiere el atributo commentado.
             // -> string date = Request.Form["Date"];
+            
 
             ViewBag.Date = date;
             ViewBag.ActivityId = activityId;
